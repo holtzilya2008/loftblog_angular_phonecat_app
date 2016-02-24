4f02d51569e3a93ef1c7e909a99325a91fd022cb
holtzilya2008 <jk124WOZ>
2016-02-24 17:55:16 +0200

    adding ngRoute navigation

--- js/controllers.js
+++ js/controllers.js
@@ -1,13 +1,53 @@
 'use strict';
 
 /* Controllers */
-var phonecatApp = angular.module('phonecatApp', []);
+var phonecatApp = angular.module('phonecatApp', ['ngRoute']);
 
-phonecatApp.controller('PhoneListCtrl',['$scope','$http', function($scope,
-$http) {     $scope.title = 'Телефоны';
+phonecatApp.config(['$routeProvider',function($routeProvide) {
+	$routeProvide
+		.when('/'.{
+			templateUrl:'html/home.html',
+			controller:'PhoneListCtrl'
+		})
+		.when('/about'.{
+			templateUrl:'html/about.html',
+			controller:'AboutCtrl'
+		})
+		.when('/contact'.{
+			templateUrl:'html/contact.html',
+			controller:'ContactCtrl'
+		})
+		.when('/other'.{
+			templateUrl:'html/other.html',
+			controller:'OtherCtrl'
+		})
+		.otherwise({
+			redirectTo:'/'
+		})
+
+	
+}])
+
+phonecatApp.controller('PhoneListCtrl',['$scope','$http', '$location', function($scope,$http,$location) {$scope.title = 'Телефоны';
 
     $http.get('phones/phones.json').success(function(data, status, headers, config) {
         $scope.phones = data;
     });
 
 }]);
+
+//About controller
+phonecatApp.controller('AboutCtrl', ['$scope','$http','$location' function($scope,$http,$location){
+	
+}])
+
+//Contact controller
+phonecatApp.controller('ContactCtrl', ['$scope','$http','$location' function($scope,$http,$location){
+}])
+
+//Other controller
+phonecatApp.controller('OtherCtrl', ['$scope','$http','$location' function($scope,$http,$location){
+}])
+//phone details controller
+phonecatApp.controller('PhoneDetailCtrl', ['$scope','$http','$location' function($scope,$http,$location){
+}])
